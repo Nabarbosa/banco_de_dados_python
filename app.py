062e697222829f8e7ce86aa2f18aac31a71641c3
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2024-10-21 10:58:06 +0000

    Salvar

--- app.py
+++ app.py
@@ -36,62 +36,23 @@ Base.metadata.create_all(bind=MEU_BANCO_ALUNO)
 
 # CRUD.
 # Creat - Insert - Salvar.
+def creat(Aluno):
+    os.system("cls || clear")
+    print("Solicitando dados para o aluno. ")
+    inserir_ra =  input("Digite seu RA: ")
+    inserir_nome =  input("Digite seu nome: ")
+    inserir_sobrenome =  input("Digite seu sobrenome: ")
+    inserir_email =  input("Digite seu e-mail: ")
+    inserir_senha =  input("Digite seu senha: ")
 
-os.system("cls || clear")
-print("Solicitando dados para o aluno. ")
-inserir_ra =  input("Digite seu RA: ")
-inserir_nome =  input("Digite seu nome: ")
-inserir_sobrenome =  input("Digite seu sobrenome: ")
-inserir_email =  input("Digite seu e-mail: ")
-inserir_senha =  input("Digite seu senha: ")
-
-aluno = Aluno(ra= inserir_ra, nome=inserir_nome, sobrenome=inserir_sobrenome, email=inserir_email, senha=inserir_senha)
-session.add(aluno)
-session.commit()
-
-# Read - Select - Consulta
-print("\nExibindo dados de todos os Alunos na tabela.")
-lista_alunos = session.query(Aluno).all()
-
-for aluno in lista_alunos:
-    print(f"{aluno.ra} - {aluno.nome} - {aluno.sobrenome} - {aluno.email} - {aluno.senha}")
-
-# U - Update - UPDATE - Atualizar
-print("\nAtualizando dados do aluno.")
-ra_aluno = input("Digite o RA do aluno que será atualizado: ")
-
-aluno = session.query(Aluno).filter_by(ra = ra_aluno).first()
-
-if aluno:
-    aluno.ra = input("Digite seu RA: ")
-    aluno.nome = input("Digite seu nome: ")
-    aluno.sobrenome = input("Digite seu sobrenome: ")
-    aluno.email = input("Digite seu e-mail: ")
-    aluno.senha = input("Digite sua senha: ")
-
-    session.commit
-else: 
-    print("Aluno não encontrado. ")
-
-# Read - Select - Consulta
-print("\nExibindo dados de todos os Alunos na tabela.")
-lista_alunos = session.query(Aluno).all()
-
-for aluno in lista_alunos:
-    print(f"{aluno.ra} - {aluno.nome} - {aluno.sobrenome} - {aluno.email} - {aluno.senha}")
-
-# D - Delete - DELETE -  Excluir
-print("\nExcluindo os dados de um aluno.")
-ra_aluno = input("Digite o RA do aluno que será excluido: ")
-
-aluno = session.query(Aluno).filter_by(ra = ra_aluno).first()
-
-if aluno:
-    session.delete(aluno)
+    aluno = Aluno(ra= inserir_ra, nome=inserir_nome, sobrenome=inserir_sobrenome, email=inserir_email, senha=inserir_senha)
+    session.add(aluno)
     session.commit()
-    print(f"Aluno {aluno.nome} excluido com sucesso!")
-else:
-    print("Aluno não encontrado.")
+
+    return Aluno
+
+dados_aluno = creat(Aluno)
+
 
 # Read - Select - Consulta
 print("\nExibindo dados de todos os Alunos na tabela.")
@@ -100,16 +61,60 @@ lista_alunos = session.query(Aluno).all()
 for aluno in lista_alunos:
     print(f"{aluno.ra} - {aluno.nome} - {aluno.sobrenome} - {aluno.email} - {aluno.senha}")
 
-# R - Read - SELECT - Consulta
-print("Consultando os dados de apenas um aluno.")
-ra_aluno = input("Digite o RA do aluno: ")
+# # U - Update - UPDATE - Atualizar
+# print("\nAtualizando dados do aluno.")
+# ra_aluno = input("Digite o RA do aluno que será atualizado: ")
 
-aluno = session.query(Aluno).filter_by(ra = ra_aluno).first()
+# aluno = session.query(Aluno).filter_by(ra = ra_aluno).first()
 
-if aluno:
-    print(f"{aluno.ra} - {aluno.nome} - {aluno.sobrenome} - {aluno.email} - {aluno.senha}")
-else:
-    print("Aluno não encontrado.")
+# if aluno:
+#     aluno.ra = input("Digite seu RA: ")
+#     aluno.nome = input("Digite seu nome: ")
+#     aluno.sobrenome = input("Digite seu sobrenome: ")
+#     aluno.email = input("Digite seu e-mail: ")
+#     aluno.senha = input("Digite sua senha: ")
+
+#     session.commit
+# else: 
+#     print("Aluno não encontrado. ")
+
+# # Read - Select - Consulta
+# print("\nExibindo dados de todos os Alunos na tabela.")
+# lista_alunos = session.query(Aluno).all()
+
+# for aluno in lista_alunos:
+#     print(f"{aluno.ra} - {aluno.nome} - {aluno.sobrenome} - {aluno.email} - {aluno.senha}")
+
+# # D - Delete - DELETE -  Excluir
+# print("\nExcluindo os dados de um aluno.")
+# ra_aluno = input("Digite o RA do aluno que será excluido: ")
+
+# aluno = session.query(Aluno).filter_by(ra = ra_aluno).first()
+
+# if aluno:
+#     session.delete(aluno)
+#     session.commit()
+#     print(f"Aluno {aluno.nome} excluido com sucesso!")
+# else:
+#     print("Aluno não encontrado.")
+
+# # Read - Select - Consulta
+# print("\nExibindo dados de todos os Alunos na tabela.")
+# lista_alunos = session.query(Aluno).all()
+
+# for aluno in lista_alunos:
+#     print(f"{aluno.ra} - {aluno.nome} - {aluno.sobrenome} - {aluno.email} - {aluno.senha}")
+
+# # R - Read - SELECT - Consulta
+# print("Consultando os dados de apenas um aluno.")
+# ra_aluno = input("Digite o RA do aluno: ")
+
+# aluno = session.query(Aluno).filter_by(ra = ra_aluno).first()
+
+# if aluno:
+#     print(f"{aluno.ra} - {aluno.nome} - {aluno.sobrenome} - {aluno.email} - {aluno.senha}")
+# else:
+#     print("Aluno não encontrado.")
 
 # Fechando conexão.
 session.close()
